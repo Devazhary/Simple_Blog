a6026a4c48c607ac6acdcc3ac7fc47db0eeb28f9
Ahmed elazhary <ahmedelazhary804@gmail.com>
2025-09-30 12:48:28 +0300

    second commit

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
-    return view('posts.index');
+    return route('posts.index');
 });
 
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
